cd459617334dd859470144d363a44c80b0e3cde9
thenorili <norilimail@gmail.com>
2022-12-15 23:15:54 -0500

    added test

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -43,29 +43,31 @@
 
 use std::fs;
 use std::env::args;
-use std::path::Path;
+use std::path;
 
-fn input_to_string() -> String
+// TODO turn input.txt import into library for other AoC puzzles
+fn args_to_vec() -> Vec<String>
 {
     let args: Vec<String> = args().collect();
-    // println!("{:?}", args);
-    debug_assert!(args.len() == 2);
-    // println!("In file {}", args[1]);
-    let path = Path::new(&args[1]);
+    return args
+}
+
+fn arg2_to_string(input: String) -> String
+{
+    let path = path::Path::new(&input);
     debug_assert!(path.exists());
 
     let contents = fs::read_to_string(path)
         .expect("Should have been able to read the file");
 
-    return contents
+    return contents;
 }
 
-fn main()
+fn find_max_run(input: String) -> usize
 {
     // let mut elves = vec![];
     let mut current: usize = 0;
     let mut max: usize = 0;
-    let input = input_to_string();
     let lines = input.lines();
     for line in lines {
         if line == "" {
@@ -80,5 +82,21 @@ fn main()
             current += cal;
         }
     }
-    println!("{}", max);
+    return max
+}
+
+fn main()
+{
+    let args = args_to_vec();
+    let input = arg2_to_string(args[1].to_owned());
+    find_max_run(input);
+}
+
+// TODO criterion to measure benchmarks
+#[test]
+fn main_test() {
+    let proj_path = env!("CARGO_MANIFEST_DIR").to_owned();
+    let input_path = proj_path + "/input.txt";
+    let input = arg2_to_string(input_path);
+    find_max_run(input);
 }
